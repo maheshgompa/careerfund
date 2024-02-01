40af629eb65749724f57e5549ee88f8378fa8349
mahesh <143725255+maheshgompa@users.noreply.github.com>
2024-02-02 04:34:15 +0530

    Update change_permissions.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+chown ec2-user:ec2-user ./start_server.sh
+chown ec2-user:ec2-user ./stop_server.sh
+chmod +x ./start_server.sh
+chmod +x ./stop_server.sh
+
 chmod +x ./start_server.sh
 chmod +x ./stop_server.sh
 
@@ -7,3 +12,4 @@ chmod +x scripts/start_server.sh
 chmod +x scripts/stop_server.sh
 
 # Additional commands if needed
+#!/bin/bash
