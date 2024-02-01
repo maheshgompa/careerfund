1b8d6e61b0bc472ccd9627a2b14af0f40a5070e0
mahesh <143725255+maheshgompa@users.noreply.github.com>
2024-02-02 03:42:51 +0530

    Create change_permissions.sh

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x /opt/codedeploy-agent/deployment-root/$CODEDEPLOY_DEPLOYMENT_ID/deployment-archive/scripts/start_server.sh
+chmod +x /opt/codedeploy-agent/deployment-root/$CODEDEPLOY_DEPLOYMENT_ID/deployment-archive/scripts/stop_server.sh
