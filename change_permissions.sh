003a0084bded6bf495ca5e7f94987fc42be62809
mahesh <143725255+maheshgompa@users.noreply.github.com>
2024-02-02 04:28:36 +0530

    Delete change_permissions.sh

--- change_permissions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x /opt/codedeploy-agent/deployment-root/$CODEDEPLOY_DEPLOYMENT_ID/deployment-archive/scripts/start_server.sh
-chmod +x /opt/codedeploy-agent/deployment-root/$CODEDEPLOY_DEPLOYMENT_ID/deployment-archive/scripts/stop_server.sh
